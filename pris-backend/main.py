1c5f671dd7f1b60e3b5138c54261e33d61b56d6c
Your Name <your.email@example.com>
2025-04-23 02:24:50 -0600

    agregando cors al backend

--- pris-backend/main.py
+++ pris-backend/main.py
@@ -4,8 +4,17 @@ from app.routes import user_router
 from app.routes import client_router
 from app.routes import date_router
 from app.routes import appointment_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router.router, prefix="/auth", tags=["Auth Routes"])
 app.include_router(user_router.router, prefix="/users", tags=["User Routes"])
